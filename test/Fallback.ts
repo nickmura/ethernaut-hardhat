7333ad15a0f1cb9d63064bb1235e17fec580821e
nickmura <nickmuradev@gmail.com>
2023-05-13 21:05:28 -0600

    initialized start of ethernaut challenges && hardhat

--- test/Fallback.ts
+++ test/Fallback.ts
@@ -5,13 +5,29 @@ import { ethers } from "hardhat"
 
 require('dotenv').config()
 
+import FallbackABI  from '../bin/contracts/Fallback.json'  
+
+const contract = '0x880d3f2a44ff7701331C223A01de19a96C11A402' // contract instance of Fallback.sol on Sepolia
+
+
+
 describe("Fallback", function () {
 
-    it('Connect to Sepolia network rpc', async function () {
-        const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/424a23ceaec54e76a1dca127e7329ee2')
+    it('Connect to Sepolia network rpc, and getBalance of my address', async function () {
+        const provider = new ethers.providers.JsonRpcProvider(process.env.API_LK)
         const signer = provider.getSigner()
-        expect(provider && signer)
+        
+
+        let balance = await provider.getBalance('0x2810FFaE7480a3D4Fea16380Aa2473340C25bF31')
+        console.log(balance)
+        expect(provider && signer && balance)
     }) 
 
+
+    it('Get contract instance of Fallback.sol', async function () {
+        const provider = new ethers.providers.JsonRpcProvider(process.env.API_LK)
+        const signer = provider.getSigner()
+
+    })
 
 })
