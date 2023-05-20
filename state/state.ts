d7179258bd8851672038eacc90392b35c389a36e
nickmura <nickmuradev@gmail.com>
2023-05-20 16:41:14 -0600

    addedd misc

--- state/state.ts
+++ state/state.ts
@@ -5,5 +5,6 @@ require('dotenv').config()
 export const player = '0x2810FFaE7480a3D4Fea16380Aa2473340C25bF31' // current sepolia address player 
 
 export const PK = process.env.S_PK
+export const API_KA= process.env.API_KA
 export const API = process.env.API_K
 export const API_LK = process.env.API_LK
--- test/Fallback.ts
+++ test/Fallback.ts
@@ -1,18 +1,21 @@
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
-import { InfuraProvider } from "@ethersproject/providers";
+import { EtherscanProvider, InfuraProvider } from "@ethersproject/providers";
+require('dotenv').config()
+
 
 import { expect } from "chai"; // testing framework thing
-import { ethers } from "hardhat"
+import { ethers, network } from "hardhat"
 
 
 
 import FallbackABI  from '../bin/contracts/Fallback.json'  
-import { player, API, API_LK } from '../state/state'
+import { player, PK, API, API_KA } from '../state/state'
+import { Signer, Wallet, providers } from "ethers";
 
-const contract = '0x880d3f2a44ff7701331C223A01de19a96C11A402' // contract instance of Fallback.sol on Sepolia
+const contractAddress = '0x880d3f2a44ff7701331C223A01de19a96C11A402' // contract instance of Fallback.sol on Sepolia
 
-
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"contribute","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"contributions","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getContribution","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"withdraw","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]
 
 describe("Fallback", function () {
 
@@ -29,9 +32,43 @@ describe("Fallback", function () {
 
 
     // it('Get contract instance of Fallback.sol', async function () {
-    //     const provider = new ethers.providers.JsonRpcProvider(API_LK)
+    //     const provider = new ethers.providers.InfuraProvider(API)
     //     const signer = provider.getSigner()
+    //     //@ts-ignore
+    //     const contract = new ethers.Contract(contractAddress, abi, signer)
 
+    //     expect(contract)
     // })
+
+
+    it('Instantiate private key', async function () {
+        let chainid = ethers.utils.hexlify(111555111)
+        console.log(chainid)
+         const setting = {apiKey: API_KA, network: chainid}         
+        const provider = new ethers.providers.AlchemyProvider(API_KA)
+
+        //@ts-ignore
+        const signer = new Wallet(PK)
+
+
+
+        expect(provider && signer)
+        
+        // let sign = await signer.sendTransaction(tx)
+        // expect(contract && sign)
+    })
+    it('Fallback contract instance, and calling receive function', async function () {
+        const provider = new ethers.providers.AlchemyProvider(API_KA)
+
+        //@ts-ignore
+        const signer = new ethers.Wallet(PK, provider)
+
+        expect(provider && signer)
+        // const contract = new ethers.Contract(contractAddress, abi, signer)
+        // const tx = await signer.sendTransaction({to: contractAddress, value: 10000000})
+        // console.log(tx)
+    })
+
+
 
 })
